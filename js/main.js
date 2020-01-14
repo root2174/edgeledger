544bf5bb8f439c115639fe93e95a9ff1fcb68b28
Lucas M <lm031719@gmail.com>
2020-01-14 14:33:23 -0300

    Smooth Scrolling With JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var map;
 
+//Google Maps
 function initMap() {
 
     const loc = { lat: 42.361145, lng: -71.057083};
@@ -9,5 +10,31 @@ function initMap() {
     zoom: 14
     });
 
-    const marker = new google.maps.Maker({position: loc, map: map})
+    const marker = new google.maps.Marker({position: loc, map: map})
 }
+
+// Sticky menu background
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        this.document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
